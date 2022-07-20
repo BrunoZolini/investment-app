befab2bd535bd4d0a99e0f057b2af6dfb715ff97
Bruno Zolini <bruno.zolini@hotmail.com>
2022-07-20 11:59:20 -0300

    feat: Styled Components Inittial configuration

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import Provider from './context/myProvider'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Registration from './pages/Registration';
-import StockList from './pages/StockList';
-import BuyAndSellStock from './pages/BuyAndSellStock';
-import DepositAndWithdraw from './pages/DepositAndWithdraw';
+import React from "react";
+import Provider from "./context/myProvider";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import StockList from "./pages/StockList";
+import BuyAndSellStock from "./pages/BuyAndSellStock";
+import DepositAndWithdraw from "./pages/DepositAndWithdraw";
+
+import GlobalStyles from "./assets/styles/global";
+import defaultTheme from "./assets/styles/themes/default";
 
 function App() {
   return (
     <div>
-      <Provider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ Login } />
-            <Route exact path="/acoes" component={ StockList } />
-            <Route exact path="/cadastro" component={ Registration } />
-            <Route exact path="/acao/:code" component={ BuyAndSellStock } />
-            <Route exact path="/banco" component={ DepositAndWithdraw } />
-          </Switch>
-        </BrowserRouter>
-      </Provider>
+      <ThemeProvider theme={defaultTheme}>
+        <Provider>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/acoes" component={StockList} />
+              <Route exact path="/cadastro" component={Registration} />
+              <Route exact path="/acao/:code" component={BuyAndSellStock} />
+              <Route exact path="/banco" component={DepositAndWithdraw} />
+            </Switch>
+            <GlobalStyles />
+          </BrowserRouter>
+        </Provider>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,13 @@
+const defaultTheme = {
+  colors: {
+    background: "#EDF2F4",
+    primary: {
+      background: "#3F3D56",
+      main: "#FF6584",
+      lighter: "#ffe2ed",
+      dark: "#b20425",
+    },
+  },
+};
+
+export default defaultTheme;
