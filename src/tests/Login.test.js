f30fad9cf365728a329955abca264cfe8189369f
Bruno Zolini <bruno.zolini@hotmail.com>
2022-07-23 15:47:23 -0300

    feat: tests configuration

---
+++ src/tests/Login.test.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import { render, screen } from "@testing-library/react";
+// import Login from '../pages/Login';
+
+describe('Hello Word', () => {
+  it('Hello Word', () => {
+    expect(true).toBe(true);
+  })
+})
